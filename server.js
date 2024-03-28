5f7fe6d514fe1c5ce59ee5d212105dc7c0553830
Anatoly Batov <play.hoffa@gmail.com>
2024-03-28 14:28:33 +0300

    commit

--- server.js
+++ server.js
@@ -3,83 +3,91 @@ import express from "express";
 import WebSocket, { WebSocketServer } from "ws";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { randomUUID } from "crypto";
+import * as crypto from "crypto";
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
-
+app.use(
+  bodyParser.json({
+    type(req) {
+      return true;
+    },
+  })
+);
 app.use((req, res, next) => {
   res.setHeader("Content-Type", "application/json");
   next();
 });
 
 const userState = [];
-
-app.post("/new-user", async (req, res) => {
-  const { name } = req.body;
-
-  if (!name) {
+app.post("/new-user", async (request, response) => {
+  if (Object.keys(request.body).length === 0) {
     const result = {
       status: "error",
-      message: "Name is required!",
+      message: "This name is already taken!",
     };
-    return res.status(400).json(result);
+    response.status(400).send(JSON.stringify(result)).end();
   }
-
+  const { name } = request.body;
   const isExist = userState.find((user) => user.name === name);
-
   if (!isExist) {
     const newUser = {
-      id: randomUUID(),
+      id: crypto.randomUUID(),
       name: name,
     };
-
     userState.push(newUser);
-
     const result = {
       status: "ok",
       user: newUser,
     };
-    return res.json(result);
+    response.send(JSON.stringify(result)).end();
   } else {
     const result = {
       status: "error",
       message: "This name is already taken!",
     };
-    return res.status(409).json(result);
+    response.status(409).send(JSON.stringify(result)).end();
   }
 });
 
 const server = http.createServer(app);
 const wsServer = new WebSocketServer({ server });
-
 wsServer.on("connection", (ws) => {
-  for (const client of wsServer.clients) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(userState));
-    }
-  }
-
-  ws.on("message", (msg) => {
+  ws.on("message", (msg, isBinary) => {
     const receivedMSG = JSON.parse(msg);
-
+    console.dir(receivedMSG);
     if (receivedMSG.type === "exit") {
-      const idx = userState.findIndex((user) => user.name === receivedMSG.user.name);
+      const idx = userState.findIndex(
+        (user) => user.name === receivedMSG.user.name
+      );
       userState.splice(idx, 1);
+      [...wsServer.clients]
+        .filter((o) => o.readyState === WebSocket.OPEN)
+        .forEach((o) => o.send(JSON.stringify(userState)));
+      return;
     }
-
-    for (const client of wsServer.clients) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(msg);
-      }
+    if (receivedMSG.type === "send") {
+      [...wsServer.clients]
+        .filter((o) => o.readyState === WebSocket.OPEN)
+        .forEach((o) => o.send(msg, { binary: isBinary }));
     }
   });
+  [...wsServer.clients]
+    .filter((o) => o.readyState === WebSocket.OPEN)
+    .forEach((o) => o.send(JSON.stringify(userState)));
 });
 
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-  console.log(`Server has been started on http://localhost:${port}`);
-});
+const bootstrap = async () => {
+  try {
+    server.listen(port, () =>
+      console.log(`Server has been started on http://localhost:${port}`)
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+bootstrap();
